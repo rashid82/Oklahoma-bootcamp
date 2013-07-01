09bd0c132028cbb88b80bc39f5a9a01dfb1180e8
rashid <rashidbao@gmail.com>
2013-07-01 15:38:37 -0400

    sdasndsnd

--- add_num.py
+++ add_num.py
@@ -1,11 +1,8 @@
+from adding import add_three
 def add_two(a,b):
-   return a+b
-
+    return a+b
 def main():
-   print ("2 + 3 = ",add_two(2,3)
-
+    print ("2 + 3 = ",add_two(2,3))
+    print ("2 + 3 + 4 = ",add_three(2,3,4))
 if __name__ == "__main__":
-main()
-
-""" This short program just adds numbers."""
-sbd,sbd
+    main()
